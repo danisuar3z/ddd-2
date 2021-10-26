dc305c8652924272fcd4a77cb55f80d256474768
Dani Suarez <suarezdanieltomas@gmail.com>
2021-10-26 01:27:31 -0300

    added docstring

--- utils.py
+++ utils.py
@@ -13,26 +13,14 @@ def lognormal(x, mu, s):
     return (1/(x*s*np.sqrt(2*np.pi))) * (np.exp(-(((np.log(x/mu))**2)/(2*s**2))))
 
 
-def extend_list(y_data, sizes, n=10000):  # , scaling=None):
-    """
-    This function creates a list to be able to plot an histogram
-    using the frequencies obtained (y_data).
-    'sizes' is the sizes pd.Series from the jacobian file
-    """
-    freqs = [round(i/y_data.sum()*n) for i in y_data]
-    extended_list = []
-    for i, freq in enumerate(freqs):
-        if freq == 0:
-            continue
-        for _ in range(freq):
-            # Append the corresponding particle size 'freq' times
-            extended_list.append(sizes.iloc[i])
-    return pd.Series(extended_list)
-
-
-# BORRADOR
-
 def load_df(contents, filename, col_names=None):
+    """
+    Recieves file contents string from Upload component.
+    Returns pandas.DataFrame or a string if unsupported file format.
+    Supported filetypes: csv, xls, xlsx. Check templates in data folder.
+    Parsing: assumes first row is headers and replaces it with
+    col_names if passed.
+    """
     _, content_string = contents.split(",")
     decoded = base64.b64decode(content_string)
 
@@ -52,3 +40,20 @@ def load_df(contents, filename, col_names=None):
         return "EXT_ERROR"
 
     return df
+
+
+def extend_list(y_data, sizes, n=10000):  # , scaling=None):
+    """
+    This function creates a list to be able to plot an histogram
+    using the frequencies obtained (y_data).
+    'sizes' is the sizes pd.Series from the jacobian file
+    """
+    freqs = [round(i/y_data.sum()*n) for i in y_data]
+    extended_list = []
+    for i, freq in enumerate(freqs):
+        if freq == 0:
+            continue
+        for _ in range(freq):
+            # Append the corresponding particle size 'freq' times
+            extended_list.append(sizes.iloc[i])
+    return pd.Series(extended_list)
